334eb448b00d47179c5c1e46288dbba0b17bdfe7
SadeghHayeri <hayerisadegh@gmail.com>
2018-11-21 19:01:57 +0330

    Add clean.sh + Move to main folder

---
+++ src/main/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm *.class
+rm *.tokens
+rm Smoola*.java
+rm *.interp
+find . -type f -name "*.class" -delete
--- src/main/run-without-intellij.sh
+++ src/main/run-without-intellij.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-rm *.class
-rm *.tokens
-rm Smoola*.java
+./clean.sh
+
 java -jar /usr/local/lib/antlr-4.7.1-complete.jar Smoola.g4
 javac *.java
 java org.antlr.v4.gui.TestRig Smoola program < ../test/$1
