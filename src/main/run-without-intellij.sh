289a687d84a091e05d11585b479631b742d67ce5
SadeghHayeri <hayerisadegh@gmail.com>
2018-11-22 16:08:34 +0330

    Fix bug for test files

--- src/main/run-without-intellij.sh
+++ src/main/run-without-intellij.sh
@@ -3,4 +3,4 @@
 
 java -jar /usr/local/lib/antlr-4.7.1-complete.jar Smoola.g4
 javac *.java
-java org.antlr.v4.gui.TestRig Smoola program < ../test/$1
+java org.antlr.v4.gui.TestRig Smoola program < ../../test/$1
