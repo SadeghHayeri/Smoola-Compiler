3294fc91a5d994ff2d032730c2279e6634b263fa
SadeghHayeri <hayerisadegh@gmail.com>
2018-12-12 15:49:53 +0330

    Move clean after run

--- run-without-intellij.sh
+++ run-without-intellij.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 export CLASSPATH=".:/usr/local/lib/antlr-4.7.1-complete.jar:$CLASSPATH"
-
-./clean.sh
 cd src/main
 
 java -jar /usr/local/lib/antlr-4.7.1-complete.jar Smoola.g4
 javac *.java
 java org.antlr.v4.gui.TestRig Smoola program < ../../test/$1
+
+cd ../../
+./clean.sh
